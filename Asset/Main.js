36c93f5a8fab305cb94a26319ecfba0bf4b781c9
Chesino <45505217+chesino@users.noreply.github.com>
2024-11-03 16:56:33 +0700

    Hunq

--- Asset/Main.js
+++ Asset/Main.js
@@ -9,29 +9,45 @@ const translations = {
         BtnDownloadCV: "Tải CV",
 
         QualificationsText: "Bằng cấp",
-        SkillsText:"Kĩ năng",
-        ProjectText:"Dự án",
-        StoreText:"Cừa hàng",
+        SkillsText: "Kĩ năng",
+        ProjectText: "Dự án",
+        StoreText: "Cửa hàng",
 
-        HideInfo:"Hiện tại đang ẩn",
+        HideInfo: "Hiện tại đang ẩn",
 
         DevText: "Lập trình",
+
+        SkillNote: "*Các đánh giá này được dựa trên đánh giá từ những người có kinh nghiệm chuyên ngành.",
+        Skil1: "Cơ điện tử",
+        Skill2: "Lập trình PLC",
+        Skill3: "Tin học văn phòng",
+        Skill4: "Lập trình Web [Front-End]",
+        Skill5: "Auto CAD",
+        Skill6: "Lập trình Arduino, Vi xử lý",
     },
     en: {
         welcomeText: "Hello",
         Fullname: "Dinh Manh Hung",
         Position: "Mechatronics Engineering",
-        Quote: "Desiring to develop skills and knowledge in mechatronics engineering, learn new technologies, and contribute to real-world projects to become an expert in the future.",
+        Quote: "*Desiring to develop skills and knowledge in mechatronics engineering, learn new technologies, and contribute to real-world projects to become an expert in the future.",
         BtnHireMe: "Hire Me",
         BtnDownloadCV: "Download CV",
 
         QualificationsText: "Qualifications",
-        SkillsText:"Skills",
-        ProjectText:"Project",
-        StoreText:"Store",
+        SkillsText: "Skills",
+        ProjectText: "Project",
+        StoreText: "Store",
 
-        HideInfo:"Currently hidden",
+        HideInfo: "Currently hidden",
         DevText: "Developer",
+
+        SkillNote: "*These evaluations are based on assessments from experienced professionals in the field.",
+        Skil1: "Mechatronics",
+        Skill2: "PLC Programming",
+        Skill3: "Office Computing",
+        Skill4: "Web Programming [Front-End]",
+        Skill5: "Auto CAD",
+        Skill6: "Arduino Programming, Microcontroller",
     },
     ja: {
         welcomeText: "こんにちは",
@@ -42,12 +58,20 @@ const translations = {
         BtnDownloadCV: "履歴書をダウンロード",
 
         QualificationsText: "資格",
-        SkillsText:"スキル",
-        ProjectText:"プロジェクト",
-        StoreText:"ショップ",
+        SkillsText: "スキル",
+        ProjectText: "プロジェクト",
+        StoreText: "ショップ",
 
-        HideInfo:"現在隠れています",
+        HideInfo: "現在隠れています",
         DevText: "開発者 ",
+
+        SkillNote: "*これらの評価は、専門分野で経験のある人の評価に基づいています。",
+        Skil1: "メカトロニクス",
+        Skill2: "PLCプログラミング",
+        Skill3: "オフィスコンピューティング",
+        Skill4: "ウェブプログラミング [フロントエンド]",
+        Skill5: "オートCAD",
+        Skill6: "Arduinoプログラミング、マイクロコントローラー",
     }
 };
 
@@ -84,8 +108,13 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Hàm áp dụng ngôn ngữ
+
 function applyLanguage(language) {
-    console.log(`Áp dụng ngôn ngữ: ${language}`);
+    var body = document.body.classList;
+    body.remove('en', 'vi', 'ja');
+
+    // Thêm lớp ngôn ngữ mới
+    body.add(language);
 
     // Lấy tất cả phần tử có thuộc tính data-translate và cập nhật nội dung
     document.querySelectorAll('[data-translate]').forEach(el => {
@@ -124,4 +153,27 @@ themeToggle.addEventListener('click', () => {
 
     // Save the new theme to localStorage
     localStorage.setItem('theme', newTheme);
+});
+
+
+// Tạo các thanh tiến trình dựa trên data-percentage
+document.querySelectorAll('.progress').forEach(function (progress) {
+    let percentage = progress.getAttribute('data-percentage');
+
+    // Tạo phần tử nhãn, thanh chứa và thanh tiến trình
+    let container = document.createElement('div');
+    container.classList.add('progress-container');
+
+
+    let barWrapper = document.createElement('div');
+    barWrapper.classList.add('progress-bar-wrapper');
+
+    let bar = document.createElement('div');
+    bar.classList.add('progress-bar');
+    bar.style.width = percentage + '%';
+
+    // Gắn các phần tử vào DOM
+    barWrapper.appendChild(bar);
+    container.appendChild(barWrapper);
+    progress.appendChild(container);
 });
