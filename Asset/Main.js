3b7452c96d02b8eceae3174e59e14caf600ad79f
Chesino <45505217+chesino@users.noreply.github.com>
2024-11-04 22:33:25 +0700

    H

--- Asset/Main.js
+++ Asset/Main.js
@@ -25,7 +25,7 @@ const translations = {
         Skill5: "Auto CAD",
         Skill6: "Lập trình Arduino, Vi xử lý",
 
-        ExpText:"Kinh nghiệm",
+        ExpText: "Kinh nghiệm",
         Exp1: "Tốt nghiệp trung học phổ thông",
         Exp2: "Tốt nghiệp Cao đẳng Cơ Điện Tử tiêu chuẩn Đức",
         Exp3: "CTY TNHH KỸ THUẬT ELECOM VIỆT NAM",
@@ -40,6 +40,11 @@ const translations = {
         <br>Giám sát hệ thống phòng cháy chữa cháy: Kiểm tra thiết bị, kiểm soát các nguồn cháy nổ, đào tạo về phòng cháy chữa cháy. 
         <br>Giám sát an toàn lao động: Nhắc nhở, phổ biến, huấn luyện an toàn cho nhà thầu thi công.
         <br>Giám sát an ninh trật tự trong quản lý dự án`,
+
+        LanguagesText: "Ngoại ngữ",
+        LanEN:"Tiếng Anh B2",
+        LanJA:"Tiếng Nhật N4",
+
     },
     en: {
         welcomeText: "Hello",
@@ -65,7 +70,7 @@ const translations = {
         Skill5: "Auto CAD",
         Skill6: "Arduino Programming, Microcontroller",
 
-        ExpText:"Experience",
+        ExpText: "Experience",
         Exp1: "High School Graduate",
         Exp2: "Graduated from German-standard Mechatronics College",
         Exp3: "ELECOM VIETNAM TECHNICAL CO., LTD",
@@ -79,8 +84,11 @@ const translations = {
         Exp5b: `Handling electrical system issues: Troubleshooting issues like power outages, equipment failures.
         <br>Supervising fire protection systems: Inspecting equipment, monitoring fire/explosion sources, providing fire protection training.
         <br>Supervising labor safety: Reminding, educating, and training safety practices for contractors.
-        <br>Supervising security and order in project management.`
+        <br>Supervising security and order in project management.`,
 
+        LanguagesText: "Languages",
+        LanEN:"English B2",
+        LanJA:"JLPT N4",
     },
     ja: {
         welcomeText: "こんにちは",
@@ -106,7 +114,7 @@ const translations = {
         Skill5: "オートCAD",
         Skill6: "Arduinoプログラミング、マイクロコントローラー",
 
-        ExpText:"経験 (けいけん)",
+        ExpText: "経験 (けいけん)",
         Exp1: "高校卒業",
         Exp2: "ドイツ基準のメカトロニクス専門学校卒業",
         Exp3: "ELECOM VIETNAM 技術有限会社",
@@ -120,8 +128,52 @@ const translations = {
         Exp5b: `電気システムの問題対応: 停電、設備故障などの問題を解決。
 <br>防火システムの監視: 機器の点検、火災や爆発源の管理、防火訓練。
 <br>労働安全の監視: 施工業者への注意喚起、教育、安全訓練。
-<br>プロジェクト管理の治安と秩序の監視。`
+<br>プロジェクト管理の治安と秩序の監視。`,
+        LanguagesText: "言語 (げんご)",
+        LanEN:"英語 B2",
+        LanJA:"JLPT N4",
+    },
+    zh: {
+        "welcomeText": "你好",
+        "Fullname": "丁孟雄",
+        "Position": "机电工程",
+        "Quote": "希望在机电工程领域提升技能和知识，学习新技术并参与实际项目，为成为未来的专家而努力。",
+        "BtnHireMe": "招聘",
+        "BtnDownloadCV": "下载简历",
 
+        "QualificationsText": "学历",
+        "SkillsText": "技能",
+        "ProjectText": "项目",
+        "StoreText": "商店",
+
+        "HideInfo": "当前隐藏",
+
+        "DevText": "编程",
+
+        "SkillNote": "*这些评估基于有行业经验的专业人士的评价。",
+        "Skill1": "机电工程",
+        "Skill2": "PLC编程",
+        "Skill3": "办公软件",
+        "Skill4": "网页编程 [前端]",
+        "Skill5": "Auto CAD",
+        "Skill6": "Arduino和微处理器编程",
+
+        "ExpText": "经验",
+        "Exp1": "高中毕业",
+        "Exp2": "德国标准机电工程专科毕业",
+        "Exp3": "越南ELECOM技术有限公司",
+        "Exp3a": "实习生",
+        "Exp3b": "学习机电工程的流程和技术",
+        "Exp4": "技术",
+        "Exp4a": "电动机的检查和维修",
+        "TimeExp5": "至今",
+        "Exp5": "IMPC设计与技术管理股份公司",
+        "Exp5a": "技术与监督",
+        "Exp5b": "处理电力系统问题：解决电力中断、设备故障等问题。<br>消防系统监控：检查设备，控制火灾源，进行消防培训。<br>职业安全监督：提醒、宣传和培训施工承包商的安全措施。<br>项目管理中的秩序与安全监控。",
+
+        LanguagesText: "語言",
+        LanEN:"英語 B2",
+        LanJA:"JLPT N4",
     }
 };
 
@@ -161,7 +213,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function applyLanguage(language) {
     var body = document.body.classList;
-    body.remove('en', 'vi', 'ja', 'cn');
+    body.remove('en', 'vi', 'ja', 'zh');
 
     // Thêm lớp ngôn ngữ mới
     body.add(language);
@@ -179,7 +231,7 @@ function updateFlag(language) {
         'vi': './Asset/icon/flag/vietnam.png',
         'en': './Asset/icon/flag/US.png',
         'ja': './Asset/icon/flag/Japan.png',
-        'cn': './Asset/icon/flag/china.png'
+        'zh': './Asset/icon/flag/china.png'
     };
     document.getElementById('currentFlag').src = flagMap[language];
 }
